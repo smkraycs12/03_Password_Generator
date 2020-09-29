4a90f218ee5679db86be412825bd2322bd80d480
Shane Kraycs <smkraycs13@yahoo.com>
2020-09-29 03:58:23 -0400

    modified func for upper and lower case letters and added func for numbers and symbols

--- script.js
+++ script.js
@@ -12,16 +12,27 @@ function writePassword() {
 
 // function for gen lowercase letters 
 function getRandomLower() {
-  return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+  const lowers = 'abcdefghijklmnopqrstuvwxyz';
+  return lowers[Math.floor(Math.random() * lowers.length)];
 }
 
 // funstion for gen uppercase letters
 function getRandomUpper() {
-  return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+  const uppers = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  return uppers[Math.floor(Math.random() * uppers.length)];
 }
 
+// func for gen random number between 0-9 
+function getRandomNumb() {
+  const numbs = '0123456789';
+  return numbs[Math.floor(Math.random() * numbs.length)];
+}
 
-
+// func for gen random symbols
+function getRandomSymbol() {
+  const symbols = '!@#$%^&*()[]{}+=<>/:;';
+  return symbols[Math.floor(Math.random() * symbols.length)];
+}
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
